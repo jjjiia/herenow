542397186754a325a80c140d6e30fbd70e240481
Jia <zhangjia@mit.edu>
2017-08-25 12:47:02 -0400

    paragraph

--- getData.js
+++ getData.js
@@ -12,7 +12,7 @@ function formatLocation(position){
     var lat = position.coords.latitude        
     var alt = position.coords.altitude 
     pub.coordinates = [lat,lng]
-    d3.select("#coordinates").html("Lat: "+lat+"<br/>Lng: "+lng+"<br/>Alt: "+alt)//+"<br/>"+speed+"<br/>"+alt+"<br/>"+heading)
+    d3.select("#coordinates").html("<strong>Lat:</strong> "+Math.round(lat*100)/100+" <strong>Lng:</strong> "+Math.round(lng*100)/100+" <strong>Alt:</strong> "+Math.round(alt*100)/100)//+"<br/>"+speed+"<br/>"+alt+"<br/>"+heading)
     return [lat,lng]
 }
 
@@ -46,18 +46,118 @@ function getCensusId(url,type,callBack){
 //realestate B25075
 var t1 = "B01003,B01002,B25002,B02001,B25003,B07201,B15003"
 var t2 = ",B08301,B08302,B08303"
-var t3 = ",B25064,B15012,B16002,B16004,B25081,B19001,B27010,B19013"
-var t4 = ",C15010,B19057,C24030"
+var t3 = ",B25064,B15012,B16002,B19001,B27010,B19013"
+var t4 = ",B19057"
 var t5 = ",B23025"
 
-var saveForLater = "B19055,B24080,B25004,B25006,B25034,B08134,B25035,B25041,B25065,B25069,B19053,B19059,B25061"
+var saveForLater = "C15010,B19055,B24080,B25004,B25006,B25034,B08134,B25035,B25041,B25065,B25069,B19053,B19059,B25061,C24030,B25081,B16004,"
 
 allTables = t1+t2+t3+t4+t5
 console.log("showing this many tables: "+allTables.split(",").length)
+
+function makeParagraph(){
+var paragraph = "<strong>You are at </strong> a place where the median age is "
++formatValues(getValue("B01002001"))+". "
+
++formatPercents(getPercent("B02001002"))+" residents are white, "
++formatPercents(getPercent("B02001003"))+" are black or african american, "
++formatPercents(getPercent("B02001004"))+" are american indian or alaska native, and "
++formatPercents(getPercent("B02001005"))+" are asian. Where "
++formatPercents(getPercent("B16002002"))+" of households speak only English"
++", and "+ formatPercents(getPercent("B07201002"))+ " lived in the same house last year. "
++formatPercents(getPercent("B08301010"))+" commute on public transportation, "
++formatPercents(getPercentSum(["B08302003","B08302002","B08302004"]))+ " leave for work before 6am. Where "
++formatPercents(getPercentSum(["B15003022","B15003023","B15003024","B15003025"]))+" of residents graduated college,"
++" their most popular majors were "+ formatValues(getTopRanked("B15012",3))+". "
++formatPercents(getPercent("B23025005"))+" are unemployed. "
++formatPercents(getPercentSum(["B27010017","B27010033","B27010050","B27010066"]))+" have no insurance coverage. "
++ "The median household income is "+ formatMoney(getValue("B19013001"))+", and "
++ formatPercents(getPercentSum(["B19001014","B19001015","B19001016","B19001017"]))+" of households make more than $100,000."
++"<i><br/><br/>todo next:<br/>"
++"Your phone is pointed in the direction of __degrees N, and the changes in front of you are ...<i>"
+d3.select("#paragraph").html(paragraph)
+}
+var colors = ["#de4645","#4dbb31","#ea4a73","#45b865","#e64821","#87b733","#b3324f","#5b821d","#a5361a","#b1a930","#d77231","#d1902e"]
+
+
+function getPercent(code){
+    var table = code.substr(0, code.length -3)
+    var codeValue = returnedData.data[Object.keys(returnedData.data)][table].estimate[code]
+    var totalCode = table+"001"
+    var totalValue = returnedData.data[Object.keys(returnedData.data)][table].estimate[totalCode]
+    var percent = codeValue/totalValue*100
+    return percent
+}
+
+function formatPercents(percent){
+    var color = colors[Math.round(Math.random()*colors.length)]
+    return "<span style=\"color:"+color+"\"><strong>"+Math.round(percent)+"%</strong></span>"
+}
+function formatMoney(money){
+    var color = colors[Math.round(Math.random()*colors.length)]
+    money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return "<span style=\"color:"+color+"; font-size = 24px\"><strong>$"+money+"</strong></span>"
+}
+function formatValues(value){
+    var color = colors[Math.round(Math.random()*colors.length)]
+    return "<span style=\"color:"+color+"\"><strong>"+value+"</strong></span>"
+}
+
+function getPercentSum(codes){
+    var sum = 0
+    for(var c in codes){
+        var code = codes[c]
+        sum+=getPercent(code)
+    }
+    return sum
+}
+function getTitle(code){
+    var table = code.substr(0, code.length -3)
+    var codeTitle = returnedData.tables[table].columns[code].name
+    return codeTitle
+}
+function getTopRanked(tableCode,ranks){
+    
+    var list = []
+    var codes = Object.keys(returnedData.data[Object.keys(returnedData.data)][tableCode].estimate)
+    var totalCode = tableCode+"001"
+    var tableCodeIndex = codes.indexOf(totalCode)
+    codes.splice(tableCodeIndex,1)
+    console.log(codes)
+    for(var c in codes){
+        var code = codes[c]
+        var codeValue = returnedData.data[Object.keys(returnedData.data)][tableCode].estimate[code]
+        list.push([code,codeValue])
+    }
+    
+    var sorted = list.sort(function(a,b){
+        return b[1]-a[1];
+    });
+    var tops = sorted.slice(0,ranks)
+    
+    var s =""
+    for(var t in tops){
+        console.log(t)
+        var code = tops[t][0]
+        var codeValue = Math.round(getPercent(code))
+        var codeTitle = getTitle(code)
+        
+        s+=codeTitle+"("+codeValue+"%), "
+    }
+    
+    s = s.slice(0,s.lastIndexOf(", "))
+    s = s.substring(0,s.lastIndexOf(","))+", and "+s.substring(s.lastIndexOf(",")+1)
+    return s
+}
+function getValue(code){
+    var table = code.substr(0, code.length -3)
+    var codeValue = returnedData.data[Object.keys(returnedData.data)][table].estimate[code]
+    return codeValue
+}
 var returnedData = null
 function formatCensusIds(json){
     var blockGroupid = "15000US"+json.Block.FIPS.slice(0,12)
-    d3.select("#censusLabelFCC").html("Block: "+json.Block.FIPS+"<br/>Block Group: "+blockGroupid ) 
+    d3.select("#censusLabelFCC").html("<strong>Block Group:</strong> "+blockGroupid ) 
     pub.censusId = blockGroupid
     getCensusData(pub.censusId,allTables)
 //    var tableName = "B01002"
@@ -103,6 +203,7 @@ function formatCensusData(data){
     returnedData = data
     var formattedData = formatDataSingle(data)
        displayDataText(formattedData)
+    makeParagraph()
 }
 function formatDataSingle(data){
     var geoid = Object.keys(data.data)
@@ -126,13 +227,11 @@ function formatDataSingle(data){
             var columnName = table["columns"][columnCode].name
             var columnValue = geoData[tableCode].estimate[columnCode]
             //&& columnValue!=0
-            if(columnValue!=undefined && columnValue!=0){
+            if(columnValue!=undefined){
                 if(columnCode.substr(columnCode.length -3)!="001"){
                     var totalCode = columnCode.substr(0, columnCode.length -3)+"001"
-                    console.log([columnCode,totalCode])
                     var totalValue = geoData[tableCode].estimate[totalCode]
                     var percent = Math.round(columnValue/totalValue*100)
-                    console.log("percent: "+percent+" column value: "+columnValue+" totalValue: "+totalValue)
                     formattedData[tableCode]["columns"][columnCode]={"title":columnName,"value":columnValue,"percent":percent}   
                 }else{
                     formattedData[tableCode]["columns"][columnCode]={"title":columnName,"value":columnValue}   
@@ -144,4 +243,4 @@ function formatDataSingle(data){
     return formattedData
 }
 
-getLocation() 
+getLocation()
--- interface.js
+++ interface.js
@@ -1,8 +1,9 @@
   d3.select("#main").append("div").attr("class","title").attr("id","title").html("you are here")
   d3.select("#main").append("div").attr("class","section").attr("id","coordinates").html("coordinates")
-  d3.select("#main").append("div").attr("class","section").attr("id","orientation").html("orientation")
+ // d3.select("#main").append("div").attr("class","section").attr("id","orientation").html("orientation")
   d3.select("#main").append("div").attr("class","section").attr("id","censusLabelFCC").html("census geography from FCC")
  // d3.select("body").append("div").attr("class","section").attr("id","censusLabelCensus").html("census geography from Census")
+  d3.select("#main").append("div").attr("class","section").attr("id","paragraph").html("data")
   d3.select("#main").append("div").attr("class","section").attr("id","data").html("data")
 
 

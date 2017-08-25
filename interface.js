20a5641e3a5e988fe620c3633f4125e7fc372c5d
Jia <zhangjia@mit.edu>
2017-08-25 10:02:51 -0400

    boot

--- interface.js
+++ interface.js
@@ -1,10 +1,10 @@
-  d3.select("body").append("div").attr("class","title").attr("id","title").html("you are here")
-  d3.select("body").append("div").attr("class","section").attr("id","coordinates").html("coordinates")
-  d3.select("body").append("div").attr("class","section").attr("id","orientation").html("orientation")
-  d3.select("body").append("div").attr("class","section").attr("id","censusLabelFCC").html("census geography from FCC")
+  d3.select("#main").append("div").attr("class","title").attr("id","title").html("you are here")
+  d3.select("#main").append("div").attr("class","section").attr("id","coordinates").html("coordinates")
+  d3.select("#main").append("div").attr("class","section").attr("id","orientation").html("orientation")
+  d3.select("#main").append("div").attr("class","section").attr("id","censusLabelFCC").html("census geography from FCC")
  // d3.select("body").append("div").attr("class","section").attr("id","censusLabelCensus").html("census geography from Census")
-  d3.select("body").append("div").attr("class","section").attr("id","data").html("data")
+  d3.select("#main").append("div").attr("class","section").attr("id","data").html("data")
 
 
-  d3.select("body").append("div").attr("class","section").attr("id","chart")
-  d3.select("body").append("div").attr("class","section").attr("id","compass")
+  d3.select("#main").append("div").attr("class","section").attr("id","chart")
+  d3.select("#main").append("div").attr("class","section").attr("id","compass")

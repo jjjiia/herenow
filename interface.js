513ec675cc7239ab022fd766a5f707bd9fae4bf1
Jia <zhangjia@mit.edu>
2017-08-27 15:32:50 -0400

    removetitle

--- interface.js
+++ interface.js
@@ -1,4 +1,4 @@
-  d3.select("#main").append("div").attr("class","title").attr("id","title").html("you are here - test, do not share")
+//  d3.select("#main").append("div").attr("class","title").attr("id","title").html("you are here - test, do not share")
   d3.select("#main").append("div").attr("class","section").attr("id","coordinates").html("coordinates")
  // d3.select("#main").append("div").attr("class","section").attr("id","orientation").html("orientation")
   d3.select("#main").append("div").attr("class","section").attr("id","censusLabelFCC").html("census geography from FCC")

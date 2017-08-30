60a143711e86d2e51a458d1123e9773441119f60
Jia <zhangjia@mit.edu>
2017-08-30 15:33:28 -0400

    bootstrap

--- interface.js
+++ interface.js
@@ -4,9 +4,9 @@
   d3.select("#main").append("div").attr("class","section").attr("id","censusLabelFCC").html("census geography from FCC")
  // d3.select("body").append("div").attr("class","section").attr("id","censusLabelCensus").html("census geography from Census")
  
-  d3.select("#main").append("div").attr("class","section").attr("id","paragraph").html("paragraph")
-  d3.select("#main").append("div").attr("class","section charts").attr("id","charts")//.html("charts")
-  d3.select("#main").append("div").attr("class","section").attr("id","data").html("data")
+  d3.select("#main").append("div").attr("class","section jumbotron").attr("id","paragraph").html("paragraph")
+  d3.select("#main").append("div").attr("class","section charts jumbotron").attr("id","charts")//.html("charts")
+  d3.select("#main").append("div").attr("class","section jumbotron").attr("id","data").html("data")
 
 
   d3.select("#main").append("div").attr("class","section").attr("id","compass")
